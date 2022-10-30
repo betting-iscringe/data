7de565649a24cafc09cfcac5afd2ef64fcf6813d
betting-iscringe <capthancock@protonmail.com>
2022-10-31 07:29:51 +0800

    10-31 HFZ Dark matches 5-18

--- script/getter.py
+++ script/getter.py
@@ -1,13 +1,15 @@
 import requests
 import json
+import pathlib
 
 if __name__ == "__main__":
 	res = requests.get("https://nasfaq.biz/api/getBettingPools")
 	data = res.json()['bettingPools']
-	with open("instructions.txt", 'r') as instructFile:
+	scriptDir = str(pathlib.Path(__file__).parent.resolve())
+	with open(scriptDir+"\\instructions.txt", 'r') as instructFile:
 		instructions = instructFile.read().splitlines()
 		out = {}
-		with open(instructions[0]+".json", "w", encoding='utf8') as outfile:
+		with open(scriptDir+"\\"+instructions[0]+".json", "w", encoding='utf8') as outfile:
 			for id in instructions[1:]:
 				if not data[id]["open"]:
 					out[id] = data[id]
